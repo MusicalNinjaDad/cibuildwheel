c5bbd19af63e55dd5da774e4f0876f1544318cb2
Mike Foster <i.am.mike.foster@gmail.com>
2024-06-04 08:44:54 +0200

    add more debug info via log.notice

--- cibuildwheel/macos.py
+++ cibuildwheel/macos.py
@@ -642,7 +642,11 @@ def build(options: Options, tmp_path: Path) -> None:
             if compatible_wheel is None:
                 # let's see exactly which bit is failing with ErrNo20 = NotADirectoryError
                 dir = build_options.output_dir.resolve()
+                log.notice(f"Wheel will go into {dir}")
+                contents = "\n".join(str(f) for f in dir.iterdir())
+                log.notice(f"Current contents:\n{contents}")
                 file = dir.joinpath(repaired_wheel.name)
+                log.notice(f"Wheel will be named {file}")
                 file.unlink(missing_ok=True)
 
                 shutil.move(str(repaired_wheel), build_options.output_dir)
